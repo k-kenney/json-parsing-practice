8d867fcaed88dff9c1b79d6ffc34218b3dd01c97
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-04-26 08:19:01 +0200

    practiced async functions, fetch, template literals, change event

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector("select");
 
-const getData = async function () {
+const getData = async function (numUsers) {
   const userRequest = await fetch(
-    "https://randomuser.me/api?results=5"
+    `https://randomuser.me/api?results=${numUsers}`
   );
   const data = await userRequest.json();
   console.log(data);
@@ -10,7 +11,7 @@ const getData = async function () {
   displayUsers(userResults);
 };
 
-getData();
+getData(1);
 
 const displayUsers = function (userResults) {
   randomFolks.innerHTML = "";
@@ -27,4 +28,9 @@ const displayUsers = function (userResults) {
         `;
     randomFolks.append(userDiv);
   }
-}
+};
+
+selectUserNumber.addEventListener("change", function (e) {
+  const numUsers = e.target.value;
+  getData(numUsers);
+});
